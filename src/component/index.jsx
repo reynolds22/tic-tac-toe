76fbc9df678fef07f9293ec78a74309555ce0b72
reynolds22 <zacharysreynolds1830@gmail.com>
2024-06-28 12:37:14 -0400

    All set up

---
+++ src/component/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TicTacToe(){
+    return (
+        <div className="container">
+            <h1>Starting</h1>
+        </div>
+    );
+};
+
+export default TicTacToe;
